40f8e05bd19015ffd566ac62e95cee3b2afd670f
krzysztof-rachwal <67062271+krzysztof-rachwal@users.noreply.github.com>
2020-09-02 17:04:51 +0200

    Add files via upload

---
+++ contact_us.js
@@ -0,0 +1,24 @@
+//1. On document Ready
+$(document).ready(function () {
+
+    // 1.1 Feedback - Success
+    contactFeedback = localStorage.getItem("contactFeedback");
+
+    if (contactFeedback === "true"){
+        $('#success_message_text').text(' The Request was sent!');
+        $('#success_message').removeClass('d-none').addClass('show');
+        localStorage.clear()
+        $("#success_message").fadeTo(1500, 1);
+        setTimeout(function(){
+            $("#success_message").fadeTo(1500, 0);
+        },5000);
+    }
+
+});
+
+//2. Feedback - Store local storage
+function contactUsFeedback() {
+    localStorage.setItem("contactFeedback","true");
+}
+
+$("#menuGetInTouch").addClass("is-active")
